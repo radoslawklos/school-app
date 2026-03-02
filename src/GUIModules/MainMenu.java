5d02db0ba5d857701748e6a0dfc7eb1d8f64eb61
RadKlo <rad.klos40@gmail.com>
2026-03-02 12:54:02 +0100

    Main menu Creation commit

--- src/GUIModules/MainMenu.java
+++ src/GUIModules/MainMenu.java
@@ -1,4 +1,34 @@
 package GUIModules;
 
-public class MainMenu {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JFrame{
+    private JLabel title;
+    private JPanel jPanel;
+    private JPanel jButtonPanel;
+    private JButton start;
+    private JButton exit;
+    private JButton settings;
+    private String title_string = "Aplikacja Szkolna";
+
+    public MainMenu(){
+        setTitle(title_string);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setContentPane(jPanel);
+        setVisible(true);
+        jButtonPanel.setLayout(new BoxLayout(jButtonPanel, BoxLayout.Y_AXIS));
+        buttonResize();
+    }
+    public void buttonResize(){
+        Dimension buttonSize = new Dimension(200, 40);
+        for (JButton b : new JButton[]{start, settings, exit}) {
+            b.setMaximumSize(buttonSize);
+            b.setPreferredSize(buttonSize);
+            b.setAlignmentX(Component.CENTER_ALIGNMENT);
+        }
+    }
+
 }
